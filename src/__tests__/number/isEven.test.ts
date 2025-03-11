3deae39b1de8b7e0d505cd9cbf5fec9fd3de8d38
Hye10 <dhye410@daum.net>
2025-03-11 22:54:41 +0900

    chore[#5]-테스트 케이스 추가 - 소수일 때 처리 추가 - 테스트 케이스 세분화

--- src/__tests__/number/isEven.test.ts
+++ src/__tests__/number/isEven.test.ts
@@ -1,17 +1,24 @@
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { isEven } from '../../number/isEven';
 
-test('isEven 테스트', () => {
-  //짝수 체크
-  expect(isEven(0)).toBe(true);
-  expect(isEven(-26)).toBe(true);
-  expect(isEven(102)).toBe(true);
-
-  //홀수 체크
-  expect(isEven(-11)).toBe(false);
-  expect(isEven(233)).toBe(false);
-
-  // Infinity , -Infinity 체크
-  expect(isEven(Infinity)).toBe(false);
-  expect(isEven(-Infinity)).toBe(false);
+describe('isEven 테스트', () => {
+  test('인자가 짝수일 때', () => {
+    expect(isEven(0)).toBe(true);
+    expect(isEven(-26)).toBe(true);
+    expect(isEven(102)).toBe(true);
+  });
+  test('인자가 홀수일 때', () => {
+    expect(isEven(-11)).toBe(false);
+    expect(isEven(233)).toBe(false);
+  });
+  test('인자가 무한대일 때', () => {
+    expect(isEven(Infinity)).toBe(false);
+    expect(isEven(-Infinity)).toBe(false);
+  });
+  test('인자가 소수일 때', () => {
+    expect(isEven(0.092)).toBe(false);
+    expect(isEven(-3.88)).toBe(false);
+    expect(isEven(1.23)).toBe(false);
+    expect(isEven(103.6)).toBe(false);
+  });
 });
--- src/number/isEven.ts
+++ src/number/isEven.ts
@@ -1,4 +1,5 @@
 export function isEven(numberToCheck: number): boolean {
+  if (!Number.isInteger(numberToCheck)) return false;
   if (numberToCheck === Infinity || numberToCheck === -Infinity) return false;
   return numberToCheck % 2 === 0;
 }

bb9b078f94d73a69296d1f05e6cf8b0dbae8dd65
Hye10 <dhye410@daum.net>
2025-03-13 14:11:24 +0900

    chore[#5]-무한한 숫자 방어 로직 수정 ||연산자로 두 개의 조건문을 넣는 것보다 Number.isFinite()를 사용하여 더욱 명료하게 수정

--- src/number/isEven.ts
+++ src/number/isEven.ts
@@ -1,5 +1,5 @@
 export function isEven(numberToCheck: number): boolean {
   if (!Number.isInteger(numberToCheck)) return false;
-  if (numberToCheck === Infinity || numberToCheck === -Infinity) return false;
+  if (!Number.isFinite(numberToCheck)) return false;
   return numberToCheck % 2 === 0;
 }

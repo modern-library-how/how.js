932780133a77d2fd4c6c679f56962da99ed80e05
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-03-13 17:49:29 +0900

    feat[#14] getSubsets 함수를 js에서 ts로 변환

--- src/array/getSubsets.ts
+++ src/array/getSubsets.ts
@@ -1,8 +1,8 @@
-function getSubsets(arr) {
-  const subsetArr = [];
-  const N = arr.length;
+function getSubsets<T>(arr: T[]): T[][] {
+  const N: number = arr.length;
+  const subsetArr: T[][] = [];
   for (let i = 0; i < 1 << N; i++) {
-    const subset = [];
+    const subset: T[] = [];
     for (let j = 0; j < N; j++) {
       if (i & (1 << j)) {
         subset.push(arr[j]);
@@ -14,3 +14,4 @@ function getSubsets(arr) {
 }
 
 console.log(getSubsets([1, 2, 3]));
+console.log(getSubsets(['hi', 'bye']));

fd80dc0c02649144e422aaefc185030b3fb6887e
Hye10 <dhye410@daum.net>
2025-03-13 21:41:23 +0900

    chore[#7] 함수명 변경 및 로직 재구현 - percentage를 적용하여 뺀 값을 구하는 함수라 좀 더 명확한 느낌을 주는 이름으로 변경

--- src/function/calculatePercent.ts
+++
@@ -1,6 +0,0 @@
-export function calculatePercent(numberToCalc: number, percentToApply: number) {
-  const percentageFactor = percentToApply / 100;
-  let calculatedValue = numberToCalc * percentageFactor;
-
-  return calculatedValue;
-}
---
+++ src/function/subtractPercentage.ts
@@ -0,0 +1,5 @@
+export function subtractPercentage(numberToCalc: number, percentToApply: number): number {
+  if (!isFinite(numberToCalc) || !isFinite(percentToApply)) return NaN;
+  const percentageFactor = percentToApply / 100;
+  return numberToCalc - numberToCalc * percentageFactor;
+}

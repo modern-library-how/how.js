f184f113afc3b9d02bdc0547f012b61883dd09b7
Hye10 <dhye410@daum.net>
2025-03-13 23:10:06 +0900

    feat[#18]-isEmpty 함수 구현

---
+++ src/function/isEmpty.ts
@@ -0,0 +1,6 @@
+export function isEmpty(value: any): boolean {
+  if (value === null || value === undefined) return true;
+  if (Array.isArray(value) || typeof value === 'string') return value.length === 0;
+  if (typeof value === 'object') return Object.keys(value).length === 0;
+  return false;
+}

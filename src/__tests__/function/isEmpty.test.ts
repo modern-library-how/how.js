6a4012df2ab383aa49bd880350a45f35c56e4a83
Hye10 <dhye410@daum.net>
2025-03-13 23:10:24 +0900

    test[#18]-isEmpty 테스트 코드 작성

---
+++ src/__tests__/function/isEmpty.test.ts
@@ -0,0 +1,50 @@
+import { describe, test, expect } from 'vitest';
+import { isEmpty } from '../../function/isEmpty';
+
+describe('isEmpty 함수 테스트', () => {
+  test('인자가 null일 때', () => {
+    expect(isEmpty(null)).toBe(true);
+  });
+
+  test('인자가 undefined일 때', () => {
+    expect(isEmpty(undefined)).toBe(true);
+  });
+
+  test('인자가 string일 때', () => {
+    expect(isEmpty('')).toBe(true);
+    expect(isEmpty('   ')).toBe(false);
+    expect(isEmpty('hello')).toBe(false);
+    expect(isEmpty('hello wrold')).toBe(false);
+  });
+
+  test('인자가 boolean일 때', () => {
+    expect(isEmpty(true)).toBe(false);
+    expect(isEmpty(false)).toBe(false);
+  });
+
+  test('인자가 배열일 때', () => {
+    expect(isEmpty([])).toBe(true);
+    expect(isEmpty([1, 2, 3])).toBe(false);
+    expect(isEmpty([{ id: 1, title: 'vitest' }])).toBe(false);
+    expect(isEmpty([true])).toBe(false);
+  });
+
+  test('인자가 객체일 때', () => {
+    expect(isEmpty({})).toBe(true);
+    expect(isEmpty({ id: 1, title: 'vitest' })).toBe(false);
+    expect(isEmpty({ flowers: ['rose', 'daisy', 'sunflower'] })).toBe(false);
+  });
+
+  test('인자가 함수일 때', () => {
+    expect(
+      isEmpty(function print(text: any) {
+        console.log(text);
+      })
+    ).toBe(false);
+  });
+  expect(
+    isEmpty(function sum(a: number, b: number) {
+      return a + b;
+    })
+  ).toBe(false);
+});

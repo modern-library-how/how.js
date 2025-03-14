28e0828a64dca5b94b8b7d455566fa79df894323
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-03-14 10:22:06 +0900

    chore[#9] filterNumber.ts 주석 추가

--- src/number/filterNumber.ts
+++ src/number/filterNumber.ts
@@ -1,3 +1,10 @@
+/**
+ * 문자열에서 숫자만 필터링하여 반환하는 함수
+ *
+ * @param {string} value
+ * @returns {number}
+ */
+
 export default function filterNumber(value: string): number {
   let regex: RegExp = /[^0-9]/g;
 

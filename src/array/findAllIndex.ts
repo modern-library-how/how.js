a3ec4f60a9310607f0ee8e9be62dc6220db92863
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-03-16 19:18:58 +0900

    feat[#26] findAllIndex 함수 생성

---
+++ src/array/findAllIndex.ts
@@ -0,0 +1,21 @@
+/**
+ * 배열에서 주어진 값과 똑같은 모든 값의 인덱스를 반환하는 함수
+ *
+ * @param arr - 주어진 배열
+ * @param equalValue - 찾기 원하는 값
+ * @returns 인덱스가 담긴 배열
+ */
+
+export function findAllIndex<T>(arr: Array<T>, equalValue: T): number[] {
+  if (Number.isNaN(equalValue)) {
+    return [];
+  }
+
+  const indexArr: number[] = [];
+  arr.forEach((value, idx) => {
+    if (value === equalValue) {
+      indexArr.push(idx);
+    }
+  });
+  return indexArr;
+}

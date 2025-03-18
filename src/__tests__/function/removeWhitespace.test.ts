8b89b60dc2db26156c702b7a5582b3ac58ceedbe
Hye10 <dhye410@daum.net>
2025-03-18 10:54:59 +0900

    test[#17]-줄바꿈, 탭에 관련한 테스트 코드 추가

--- src/__tests__/function/removeWhitespace.test.ts
+++ src/__tests__/function/removeWhitespace.test.ts
@@ -4,7 +4,7 @@ import { removeWhitespace } from '../../function/removeWhitespace';
 describe('removeWhitespace 함수 테스트', () => {
   test('공백문자만 주어졌을 때', () => {
     expect(removeWhitespace('')).toBe('');
-    expect(removeWhitespace('     ')).toBe('');
+    expect(removeWhitespace('          ')).toBe('');
     expect(removeWhitespace(' ')).toBe('');
   });
 
@@ -18,5 +18,17 @@ describe('removeWhitespace 함수 테스트', () => {
     expect(removeWhitespace('h e l l o')).toBe('hello');
     expect(removeWhitespace('Fryderyk Chopin')).toBe('FryderykChopin');
     expect(removeWhitespace('here you are')).toBe('hereyouare');
+  });
+
+  test('문자열 사이에 공백이 주어졌을 때', () => {
+    expect(removeWhitespace('h e l l o')).toBe('hello');
+    expect(removeWhitespace('Fryderyk Chopin')).toBe('FryderykChopin');
+    expect(removeWhitespace('here you are')).toBe('hereyouare');
+  });
+
+  test('공백, 탭, 줄바꿈이 주어졌을 때', () => {
+    expect(removeWhitespace('hello \t world\n')).toBe('helloworld');
+    expect(removeWhitespace(' \t hello \n world \t ')).toBe('helloworld');
+    expect(removeWhitespace('  line1\nline2  ')).toBe('line1line2');
   });
 });

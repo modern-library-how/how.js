33532e3ed59ad5db407e95affbbe0d97847379ea
Hye10 <dhye410@daum.net>
2025-03-18 19:10:04 +0900

    chore[#5] if문 하나로 합치기

--- src/number/isEven.ts
+++ src/number/isEven.ts
@@ -1,5 +1,4 @@
 export function isEven(numberToCheck: number): boolean {
-  if (!Number.isInteger(numberToCheck)) return false;
-  if (!Number.isFinite(numberToCheck)) return false;
+  if (!Number.isInteger(numberToCheck) || !Number.isFinite(numberToCheck)) return false;
   return numberToCheck % 2 === 0;
 }

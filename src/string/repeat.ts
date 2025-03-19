cbbf3021cfe8a35ecac4b387b354ce2133aa106a
UrePu <darkin31313@gmail.com>
2025-03-19 10:39:54 +0900

    feat:repeat test 작성

--- src/string/repeat.ts
+++ src/string/repeat.ts
@@ -1,5 +1,11 @@
-// 문자열을 지정된 횟수만큼 반복 ('abc'.repeat(3) → 'abcabcabc')
-
+/**
+ * 문자열을 지정된 횟수만큼 반복합니다.
+ * 예: repeat('abc', 3) → 'abcabcabc'
+ *
+ * @param str 반복할 문자열
+ * @param count 반복 횟수
+ * @returns 반복된 문자열
+ */
 export function repeat(str: string, count: number): string {
   let repeated = '';
   for (let i = 0; i < count; i++) {
